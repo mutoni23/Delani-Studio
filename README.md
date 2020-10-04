dd3ddbd07d7f07f528e835edcb0cef848958101d
mutoni vivine <mutoniv5@gmail.com>
2020-10-04 22:42:24 +0200

    add title of the project

---
+++ README.md
@@ -0,0 +1,29 @@
+# Delani-studio
+
+
+## Description
+This project and its site show what Delani-studio does,in terms of services they offer to its customers,
+the site also allows users to navigate throughout the site and see what Delani-studio does 
+by showing pictures or the images in terms of what we do section.
+And in case of  issues,suggestions or feedback there is also a form to fill.
+
+## Setup/Installation Requirements
+* To access this document first you need to go on your github account,
+* Then search for the project name,
+* You clone it to your laptop/desktop,
+* use a web browser to open it.  you can also use text editor like visual studio code
+
+
+## Technologies Used
+To make this project works more effectively, It is by the help of Html, CSS and Javascript programming languages(bootstrap,jqueries).
+
+## Support and contact details
+YOu need informations or suggestions please don't hesitate to reach out to me :
+* contact: +250782025869
+*Twitter: @Umutonivivine
+*Email: mutoniv5@gmail.com
+Github: mutoni23
+
+### License
+Copyright (c) {2020} Done by me
+ 
---
+++ js/scripts.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    $('.mon').mouseover(function() {
+     $('#tues').show();
+      $('#wedn').show();
+ })
+ $('.mon').mouseout(function() {
+      $('#tues').hide();
+      $('#wedn').hide();
+ })
+    });
+    $(document).ready(function(){
+     $('.thur').mouseover(function() {
+      $('.fri').show();
+       $('.sat').show();
+  })
+  $('.thur').mouseout(function() {
+       $('.fri').hide();
+       $('.sat').hide();
+  })
+     });
+     $(document).ready(function(){
+         $('.sun').mouseover(function() {
+          $('#jan').show();
+           $('#feb').show();
+      })
+      $('.sun').mouseout(function() {
+           $('#jan').hide();
+           $('#feb').hide();
+      })
+         });
+         $(document).ready(function(){
+             $('.march').mouseover(function() {
+              $('.april').show();
+               $('.may').show();
+          })
+          $('.march').mouseout(function() {
+               $('.april').hide();
+               $('.may').hide();
+          })
+             });
+             $(document).ready(function(){
+                 $('.june').mouseover(function() {
+                  $('.july').show();
+                   $('#augst').show();
+              })
+              $('.june').mouseout(function() {
+                   $('.july').hide();
+                   $('#augst').hide();
+              })
+                 });
+                 $(document).ready(function(){
+                     $('.sept').mouseover(function() {
+                      $('.oct').show();
+                       $('.nov').show();
+                  })
+                  $('.sept').mouseout(function() {
+                       $('.oct').hide();
+                       $('.nov').hide();
+                  })
+                     });
+                     $(document).ready(function(){
+                         $('.dec').mouseover(function() {
+                          $('.twenty').show();
+                           $('.one').show();
+                      })
+                      $('.dec').mouseout(function() {
+                           $('.twenty').hide();
+                           $('.one').hide();
+                      })
+                         });
+                         $(document).ready(function(){
+                             $('.believe').mouseover(function() {
+                              $('.decade').show();
+                               $('.two').show();
+                          })
+                          $('.believe').mouseout(function() {
+                               $('.decade').hide();
+                               $('.two').hide();
+                          })
+                             });
+
+
+                             $(document).ready(function () {
+                                $('.three').click(function () {
+                                     $('.text-three').show();
+                                     $('.three').hide();
+                                });
+                                $('.text-three').click(function () {
+                                     $('.three').show();
+                                     $('.text-three').hide();
+                                });
+                           });
+                           $(document).ready(function () {
+                                $('.four').click(function () {
+                                     $('.text-four').show()
+                                     $('.four').hide()
+                                })
+                                $('.text-four').click(function () {
+                                     $('.four').show()
+                                     $('.text-four').hide()
+                                });
+                           });
+                           $(document).ready(function () {
+                                $('.five').click(function () {
+                                     $('.text-five').show()
+                                     $('.five').hide()
+                                })
+                                $('.text-five').click(function () {
+                                     $('.five').show()
+                                     $('.text-five').hide()
+                                });
+                            });
+
+                             
